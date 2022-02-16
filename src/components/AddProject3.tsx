271f05716c06270786cdbe3b605188c1739557db
gooriiie <fkstldghks123@naver.com>
2022-02-16 16:16:05 +0900

    feat : add project3 UI

--- src/components/AddProject3.tsx
+++ src/components/AddProject3.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View, Text, StyleSheet, ScrollView, SectionList } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { scrollTo } from "react-native-reanimated";
 
 export default function AddProject3({ navigation }: any) {
     const [switch1, setSwitch1] = useState(0);
@@ -29,62 +30,74 @@ export default function AddProject3({ navigation }: any) {
                 {
                     name: "1구 스위치",
                     count: switch1,
-                    _onPlusPress: function(){
-                        setSwitch1(switch1 + 1)
+                    _onPlusPress: function () {
+                        setSwitch1(switch1 + 1);
                     },
-                    _onMinusPress: function() {
-                        setSwitch1(switch1 - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (switch1 > 0) {
+                            setSwitch1(switch1 - 1);
+                        }
+                    },
                 },
                 {
                     name: "2구 스위치",
                     count: switch2,
-                    _onPlusPress: function(){
-                        setSwitch2(switch2 + 1)
+                    _onPlusPress: function () {
+                        setSwitch2(switch2 + 1);
                     },
-                    _onMinusPress: function() {
-                        setSwitch2(switch2 - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (switch2 > 0) {
+                            setSwitch2(switch2 - 1);
+                        }
+                    },
                 },
                 {
                     name: "3구 스위치",
                     count: switch3,
-                    _onPlusPress: function(){
-                        setSwitch3(switch3 + 1)
+                    _onPlusPress: function () {
+                        setSwitch3(switch3 + 1);
                     },
-                    _onMinusPress: function() {
-                        setSwitch3(switch3 - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (switch3 > 0) {
+                            setSwitch3(switch3 - 1);
+                        }
+                    },
                 },
                 {
                     name: "4구 스위치",
                     count: switch4,
-                    _onPlusPress: function(){
-                        setSwitch4(switch1 + 1)
+                    _onPlusPress: function () {
+                        setSwitch4(switch4 + 1);
                     },
-                    _onMinusPress: function() {
-                        setSwitch4(switch1 - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (switch4 > 0) {
+                            setSwitch4(switch4 - 1);
+                        }
+                    },
                 },
                 {
                     name: "5구 스위치",
-                    count: switch4,
-                    _onPlusPress: function(){
-                        setSwitch5(switch1 + 1)
+                    count: switch5,
+                    _onPlusPress: function () {
+                        setSwitch5(switch5 + 1);
                     },
-                    _onMinusPress: function() {
-                        setSwitch5(switch1 - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (switch5 > 0) {
+                            setSwitch5(switch5 - 1);
+                        }
+                    },
                 },
                 {
                     name: "6구 스위치",
                     count: switch6,
-                    _onPlusPress: function(){
-                        setSwitch6(switch1 + 1)
+                    _onPlusPress: function () {
+                        setSwitch6(switch6 + 1);
                     },
-                    _onMinusPress: function() {
-                        setSwitch6(switch1 - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (switch6 > 0) {
+                            setSwitch6(switch6 - 1);
+                        }
+                    },
                 },
             ],
         },
@@ -94,42 +107,50 @@ export default function AddProject3({ navigation }: any) {
                 {
                     name: "1구 콘센트",
                     count: powerSocket1,
-                    _onPlusPress: function(){
-                        setPowerSocket1(powerSocket1 + 1)
+                    _onPlusPress: function () {
+                        setPowerSocket1(powerSocket1 + 1);
                     },
-                    _onMinusPress: function() {
-                        setPowerSocket1(powerSocket1 - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (powerSocket1 > 0) {
+                            setPowerSocket1(powerSocket1 - 1);
+                        }
+                    },
                 },
                 {
                     name: "2구 콘센트",
                     count: powerSocket2,
-                    _onPlusPress: function(){
-                        setPowerSocket2(powerSocket2 + 1)
+                    _onPlusPress: function () {
+                        setPowerSocket2(powerSocket2 + 1);
                     },
-                    _onMinusPress: function() {
-                        setPowerSocket2(powerSocket2 - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (powerSocket2 > 0) {
+                            setPowerSocket2(powerSocket2 - 1);
+                        }
+                    },
                 },
                 {
                     name: "4구 콘센트",
                     count: powerSocket4,
-                    _onPlusPress: function(){
-                        setPowerSocket4(powerSocket4 + 1)
+                    _onPlusPress: function () {
+                        setPowerSocket4(powerSocket4 + 1);
                     },
-                    _onMinusPress: function() {
-                        setPowerSocket4(powerSocket4 - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (powerSocket4 > 0) {
+                            setPowerSocket4(powerSocket4 - 1);
+                        }
+                    },
                 },
                 {
                     name: "6구 콘센트",
                     count: powerSocket6,
-                    _onPlusPress: function(){
-                        setPowerSocket6(powerSocket6 + 1)
+                    _onPlusPress: function () {
+                        setPowerSocket6(powerSocket6 + 1);
                     },
-                    _onMinusPress: function() {
-                        setPowerSocket6(powerSocket6 - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (powerSocket6 > 0) {
+                            setPowerSocket6(powerSocket6 - 1);
+                        }
+                    },
                 },
             ],
         },
@@ -139,24 +160,28 @@ export default function AddProject3({ navigation }: any) {
                 {
                     name: "정온식 감지기",
                     count: sensorConstant,
-                    _onPlusPress: function(){
-                        setSensorConstant(sensorConstant + 1)
+                    _onPlusPress: function () {
+                        setSensorConstant(sensorConstant + 1);
                     },
-                    _onMinusPress: function() {
-                        setSensorConstant(sensorConstant - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (sensorConstant > 0) {
+                            setSensorConstant(sensorConstant - 1);
+                        }
+                    },
                 },
                 {
                     name: "변온식 감지기",
                     count: sensorAlternative,
-                    _onPlusPress: function(){
-                        setSensorAlternative(sensorAlternative + 1)
+                    _onPlusPress: function () {
+                        setSensorAlternative(sensorAlternative + 1);
                     },
-                    _onMinusPress: function() {
-                        setSensorAlternative(sensorAlternative - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (sensorAlternative > 0) {
+                            setSensorAlternative(sensorAlternative - 1);
+                        }
+                    },
                 },
-            ]
+            ],
         },
         {
             title: "조명",
@@ -164,36 +189,44 @@ export default function AddProject3({ navigation }: any) {
                 {
                     name: "라인조명",
                     count: lightLine,
-                    _onPlusPress: function(){
-                        setLightLine(lightLine + 1)
+                    _onPlusPress: function () {
+                        setLightLine(lightLine + 1);
                     },
-                    _onMinusPress: function() {
-                        setLightLine(lightLine - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (lightLine > 0) {
+                            setLightLine(lightLine - 1);
+                        }
+                    },
                 },
                 {
                     name: "메인조명",
                     count: lightMain,
-                    _onPlusPress: function(){
-                        setLightMain(lightMain + 1)
+                    _onPlusPress: function () {
+                        setLightMain(lightMain + 1);
                     },
-                    _onMinusPress: function() {
-                        setLightMain(lightMain - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (lightMain > 0) {
+                            setLightMain(lightMain - 1);
+                        }
+                    },
                 },
                 {
                     name: "서브조명",
                     count: lightSub,
-                    _onPlusPress: function(){
-                        setLightSub(lightSub + 1)
+                    _onPlusPress: function () {
+                        setLightSub(lightSub + 1);
                     },
-                    _onMinusPress: function() {
-                        setLightSub(lightSub - 1)
-                    }
+                    _onMinusPress: function () {
+                        if (lightSub > 0) {
+                            setLightSub(lightSub - 1);
+                        }
+                    },
                 },
-            ]
+            ],
         },
     ];
+
+    const sectionlist = useRef();
 
     return (
         <View style={styles.container}>
@@ -205,6 +238,31 @@ export default function AddProject3({ navigation }: any) {
             </View>
             <View style={styles.moveButtonArea}>
                 <Text style={styles.movetext}>이동</Text>
+                <ScrollView
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                >
+                    <TouchableOpacity
+                        style={styles.moveButton}
+                    >
+                        <Text style={styles.moveButtonText}>스위치</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.moveButton}
+                    >
+                        <Text style={styles.moveButtonText}>콘센트</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.moveButton}
+                    >
+                        <Text style={styles.moveButtonText}>감지기</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.moveButton}
+                    >
+                        <Text style={styles.moveButtonText}>조명</Text>
+                    </TouchableOpacity>
+                </ScrollView>
             </View>
             <View style={styles.scrollArea}>
                 <SectionList
@@ -238,7 +296,8 @@ export default function AddProject3({ navigation }: any) {
                             </View>
                         );
                     }}
-                    // keyExtractor={(item, index) => item + index}
+                    keyExtractor={(item, index) => item.name + index}
+                    showsVerticalScrollIndicator={false}
                 />
             </View>
         </View>
@@ -263,6 +322,7 @@ const styles = StyleSheet.create({
     },
     subtitleArea: {
         flex: 0.5,
+        justifyContent: 'center'
     },
     title: {
         fontSize: 30,
@@ -275,14 +335,34 @@ const styles = StyleSheet.create({
     },
     moveButtonArea: {
         flex: 1,
+        flexDirection: "row",
         // borderBottomColor: "black",
         // borderWidth: 2,
-        paddingTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     movetext: {
         color: "black",
         fontSize: 16,
         fontWeight: "bold",
+        marginRight: 10
+    },
+    moveButton: {
+        color: 'white',
+        borderColor: '#FF7800',
+        borderWidth: 2,
+        borderRadius: 50,
+        width: 80,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 5,
+        marginRight: 5
+    },
+    moveButtonText: {
+        color: '#ff7800',
+        fontSize: 16,
+        fontWeight: 'bold'
     },
     scrollArea: {
         flex: 7,
@@ -310,3 +390,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
     },
 });
+function scrollToLocation(): void {
+    throw new Error("Function not implemented.");
+}
+
