a6dceca0c91afe3a72eb74f1cee6b138b7373a00
gooriiie <fkstldghks123@naver.com>
2022-02-16 11:54:15 +0900

    feat : add project3 UI

--- src/components/AddProject3.tsx
+++ src/components/AddProject3.tsx
@@ -1,27 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, ScrollView, SectionList } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import SolidButton from "./SolidButton";
-
-const sectionDatas = [
-    {
-        title: "스위치",
-        data: [
-            "1구 스위치",
-            "2구 스위치",
-            "3구 스위치",
-            "4구 스위치",
-            "5구 스위치",
-            "6구 스위치",
-        ],
-    },
-    {
-        title: "콘센트",
-        data: ["1구 콘센트", "2구 콘센트", "4구 콘센트", "6구 콘센트"],
-    },
-    { title: "감지기", data: ["정온식 감지기", "변온식 감지기"] },
-    { title: "조명", data: ["라인조명", "메인조명", "서브조명"] },
-];
 
 export default function AddProject3({ navigation }: any) {
     const [switch1, setSwitch1] = useState(0);
@@ -42,6 +21,179 @@ export default function AddProject3({ navigation }: any) {
     const [lightLine, setLightLine] = useState(0);
     const [lightMain, setLightMain] = useState(0);
     const [lightSub, setLightSub] = useState(0);
+
+    const sectionDatas = [
+        {
+            title: "스위치",
+            data: [
+                {
+                    name: "1구 스위치",
+                    count: switch1,
+                    _onPlusPress: function(){
+                        setSwitch1(switch1 + 1)
+                    },
+                    _onMinusPress: function() {
+                        setSwitch1(switch1 - 1)
+                    }
+                },
+                {
+                    name: "2구 스위치",
+                    count: switch2,
+                    _onPlusPress: function(){
+                        setSwitch2(switch2 + 1)
+                    },
+                    _onMinusPress: function() {
+                        setSwitch2(switch2 - 1)
+                    }
+                },
+                {
+                    name: "3구 스위치",
+                    count: switch3,
+                    _onPlusPress: function(){
+                        setSwitch3(switch3 + 1)
+                    },
+                    _onMinusPress: function() {
+                        setSwitch3(switch3 - 1)
+                    }
+                },
+                {
+                    name: "4구 스위치",
+                    count: switch4,
+                    _onPlusPress: function(){
+                        setSwitch4(switch1 + 1)
+                    },
+                    _onMinusPress: function() {
+                        setSwitch4(switch1 - 1)
+                    }
+                },
+                {
+                    name: "5구 스위치",
+                    count: switch4,
+                    _onPlusPress: function(){
+                        setSwitch5(switch1 + 1)
+                    },
+                    _onMinusPress: function() {
+                        setSwitch5(switch1 - 1)
+                    }
+                },
+                {
+                    name: "6구 스위치",
+                    count: switch6,
+                    _onPlusPress: function(){
+                        setSwitch6(switch1 + 1)
+                    },
+                    _onMinusPress: function() {
+                        setSwitch6(switch1 - 1)
+                    }
+                },
+            ],
+        },
+        {
+            title: "콘센트",
+            data: [
+                {
+                    name: "1구 콘센트",
+                    count: powerSocket1,
+                    _onPlusPress: function(){
+                        setPowerSocket1(powerSocket1 + 1)
+                    },
+                    _onMinusPress: function() {
+                        setPowerSocket1(powerSocket1 - 1)
+                    }
+                },
+                {
+                    name: "2구 콘센트",
+                    count: powerSocket2,
+                    _onPlusPress: function(){
+                        setPowerSocket2(powerSocket2 + 1)
+                    },
+                    _onMinusPress: function() {
+                        setPowerSocket2(powerSocket2 - 1)
+                    }
+                },
+                {
+                    name: "4구 콘센트",
+                    count: powerSocket4,
+                    _onPlusPress: function(){
+                        setPowerSocket4(powerSocket4 + 1)
+                    },
+                    _onMinusPress: function() {
+                        setPowerSocket4(powerSocket4 - 1)
+                    }
+                },
+                {
+                    name: "6구 콘센트",
+                    count: powerSocket6,
+                    _onPlusPress: function(){
+                        setPowerSocket6(powerSocket6 + 1)
+                    },
+                    _onMinusPress: function() {
+                        setPowerSocket6(powerSocket6 - 1)
+                    }
+                },
+            ],
+        },
+        {
+            title: "감지기",
+            data: [
+                {
+                    name: "정온식 감지기",
+                    count: sensorConstant,
+                    _onPlusPress: function(){
+                        setSensorConstant(sensorConstant + 1)
+                    },
+                    _onMinusPress: function() {
+                        setSensorConstant(sensorConstant - 1)
+                    }
+                },
+                {
+                    name: "변온식 감지기",
+                    count: sensorAlternative,
+                    _onPlusPress: function(){
+                        setSensorAlternative(sensorAlternative + 1)
+                    },
+                    _onMinusPress: function() {
+                        setSensorAlternative(sensorAlternative - 1)
+                    }
+                },
+            ]
+        },
+        {
+            title: "조명",
+            data: [
+                {
+                    name: "라인조명",
+                    count: lightLine,
+                    _onPlusPress: function(){
+                        setLightLine(lightLine + 1)
+                    },
+                    _onMinusPress: function() {
+                        setLightLine(lightLine - 1)
+                    }
+                },
+                {
+                    name: "메인조명",
+                    count: lightMain,
+                    _onPlusPress: function(){
+                        setLightMain(lightMain + 1)
+                    },
+                    _onMinusPress: function() {
+                        setLightMain(lightMain - 1)
+                    }
+                },
+                {
+                    name: "서브조명",
+                    count: lightSub,
+                    _onPlusPress: function(){
+                        setLightSub(lightSub + 1)
+                    },
+                    _onMinusPress: function() {
+                        setLightSub(lightSub - 1)
+                    }
+                },
+            ]
+        },
+    ];
 
     return (
         <View style={styles.container}>
@@ -69,21 +221,24 @@ export default function AddProject3({ navigation }: any) {
                     renderItem={({ item, index, section }) => {
                         return (
                             <View style={styles.itemView}>
-                                <Text>{item}</Text>
+                                <Text>{item.name}</Text>
                                 <TouchableOpacity
                                     style={styles.plusMinusButton}
+                                    onPress={() => item._onMinusPress()}
                                 >
                                     <Text>-</Text>
                                 </TouchableOpacity>
-                                <Text>0</Text>
+                                <Text>{item.count}</Text>
                                 <TouchableOpacity
                                     style={styles.plusMinusButton}
+                                    onPress={() => item._onPlusPress()}
                                 >
                                     <Text>+</Text>
                                 </TouchableOpacity>
                             </View>
                         );
                     }}
+                    // keyExtractor={(item, index) => item + index}
                 />
             </View>
         </View>
